0de7157bd8d73269618c0b73b2fc4fc2552a539f
Sean Wong <seanwong24@live.com>
2021-11-30 17:33:46 +0000

    implement getFileContent

--- src/controllers/file/file.controller.ts
+++ src/controllers/file/file.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import * as directoryTree from 'directory-tree';
+import { readFile } from 'fs/promises';
 
 @Controller('file')
 export class FileController {
   @Get('tree')
   getFileTree() {
     return directoryTree('./data');
+  }
+
+  @Get()
+  async getFileContent(@Query('path') path: string) {
+    return await readFile(`./data/${path}`, { encoding: 'utf-8' });
   }
 }
