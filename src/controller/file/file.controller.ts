6b4ef366e363be7e88ce8e29e402e302cfb7a7fb
Sean Wong <seanwong24@live.com>
2021-12-01 17:46:01 +0000

    user specified getFileTree

--- src/controller/file/file.controller.ts
+++ src/controller/file/file.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 import * as directoryTree from 'directory-tree';
 import { readFile } from 'fs/promises';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '@repository/user/user.service';
 
 @Controller('file')
 export class FileController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
   @Get('tree')
-  getFileTree() {
-    return directoryTree('./data');
+  async getFileTree(@Req() request: Request) {
+    const token = request.cookies['access_token'];
+    const _id = this.jwtService.decode(token)?.['_id'];
+    const user = (await this.userService.find({ _id }))?.[0];
+
+    const fileTree = directoryTree('./data');
+    fileTree.children = fileTree.children.filter(
+      (child) => child.name === user?.username || child.name === 'public',
+    );
+    return fileTree;
   }
 
   @Get()
