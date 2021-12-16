56182531c5bc43fbc89ad929aeb7f4ae2b1039c0
Sean Wong <seanwong24@live.com>
2021-12-16 16:42:14 +0000

    modify CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3333',
+    origin: /http:\/\/localhost*/,
     credentials: true,
   });
   await app.listen(5000);
